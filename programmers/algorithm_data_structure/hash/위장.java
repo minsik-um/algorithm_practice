d55f88870209b6236cec025111447c6ab91a6d55
minsik-um <ekrmdhs95@gmail.com>
2020-04-09 14:06:15 +0900

    수정: 프로그래머스 / 해시맵 / 위장(java) : 함수형 프로그래밍 간단 설명 추가

--- programmers/algorithm_data_structure/hash/위장.java
+++ programmers/algorithm_data_structure/hash/위장.java
@@ -26,6 +26,51 @@ public class 위장 {
         return answer;
     }
 
+    /*
+    출처: https://swiftymind.tistory.com/108
+    함수형 프로그래밍
+    부수 효과를 없애고 순수 함수를 만들어 모듈화 수준을 높이는 프로그래밍 패러다임
+
+    부수 효과 : 주어진 값 이외의 외부 변수 및 프로그램 실행에 영향을 끼치는 것을 말함
+    순수 함수 : 모든 입력이 입력으로만, 모든 출력이 출력으로만 사용
+    비순수 함수  : 숨겨진 입력이나 출력이 존재
+
+    함수형 프로그래밍은 순수 함수를 지향, 최대한 숨겨진 입력과 출력을 제거하여
+    가능한 코드를 입력과 출력의 관계로 사용
+
+    객체 지향 프로그래밍은 명령형 프로그래밍이다. 데이터를 '어떻게' 처리할지
+    명령을 통해 풀어나갔다면, 함수형 프로그래밍은 선언적 함수를 통해
+    '무엇을' 풀어나갈지 결정한다.
+
+    List<String> myList =
+                Arrays.asList("c1", "a2", "b3", "4", "5");
+ 
+    // 기존방식
+    for(int i=0; i<myList.size(); i++){
+        String s = myList.get(i);
+        if(s.startsWith("c")){
+            System.out.println(s.toUpperCase());
+        }
+    }
+
+    // stream API를 이용한 방식
+    myList.stream()
+            .filter(s -> s.startsWith("c"))
+            .map(String::toUpperCase)
+            .forEach(System.out::println);
+ 
+    함수형 프로그래밍에서 함수는 1급 객체여야 한다.
+    특정 언어의 일급객체(first-class citizens, entity)는
+    다른 객체들에 적용 가능한 연산을 모두 지원하는 객체다.
+
+    - 변수나 데이터 구조 안에 담을 수 있다.
+    - 파라미터로 전달할 수 있다.
+    - 반환값(return value)으로 사용할 수 있다.
+    - 할당에 사용된 이름과 관계없이 고유한 구별이 가능하다.
+    - 동적으로 프로퍼티 할당이 가능하다.
+    - 기존 데이터의 불변성(Immutable)
+    */
+
     public int best_solution(String[][] clothes) {
         /*
         JAVA 8부터 추가된 함수형 프로그래밍(Stream API) 적용
